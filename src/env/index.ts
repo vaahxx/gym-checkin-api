bba918cc0836953a6893c7a68d97e17279923471
Valentina Garavaglia <vaahxx@gmail.com>
2023-07-26 12:15:07 -0400

    chore: configured alias

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,17 +1,17 @@
-import 'dotenv/config';
-import { z } from 'zod';
+import 'dotenv/config'
+import { z } from 'zod'
 
 const envSchema = z.object({
-    NODE_ENV: z.enum(['dev', 'production', 'test']).default('dev'),
-    PORT: z.coerce.number().default(3333),
-    // DATABASE_URL: z.string(),
+  NODE_ENV: z.enum(['dev', 'production', 'test']).default('dev'),
+  PORT: z.coerce.number().default(3333),
+  // DATABASE_URL: z.string(),
 })
 
-const _env = envSchema.safeParse(process.env);
+const _env = envSchema.safeParse(process.env)
 
 if (!_env.success) {
-    console.error('❌ Invalid environment variables', _env.error.format());
-    throw new Error('Invalid environment variables')
+  console.error('❌ Invalid environment variables', _env.error.format())
+  throw new Error('Invalid environment variables')
 }
 
-export const env = _env.data;
+export const env = _env.data
